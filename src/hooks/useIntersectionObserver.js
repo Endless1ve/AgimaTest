ff6a753e4185c7b348b195a1192559404da53aa7
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-24 14:46:22 +0300

    fix(custom): deleted import from hook

--- src/hooks/useIntersectionObserver.js
+++ src/hooks/useIntersectionObserver.js
@@ -1,5 +1,3 @@
-import { onMounted } from "vue";
-
 export function useIntersectionObserver(observerRef, callback) {
   function observeElement(el) {
     const observer = new IntersectionObserver(([entry]) => {
