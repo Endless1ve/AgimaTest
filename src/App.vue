57dc3f0ee73995609a310a93360b5eb3045308f2
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-24 13:49:06 +0300

    feat(custom): added post list and fixed app styles

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,11 @@
 <style lang="scss">
   @import "@/assets/styles/normalize.scss";
   @import "@/assets/styles/fonts.scss";
+
   #app {
     font-family: $main-fonts;
+    background-color: $second-ligth-color;
+    width: 100vw;
+    height: 100vh;
   }
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div class="postList"></div>
+</template>
+
+<style lang="scss" scoped>
+  .postList {
+    padding: 10px;
+    align-items: center;
+    @include flex-column;
+  }
+</style>
