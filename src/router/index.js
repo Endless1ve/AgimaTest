83ab7ee4360dfd91d25d598bdd29c440e6c2add0
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-25 17:30:01 +0300

    feat(custom): added navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+import { useAuthStore } from "@/store/auth";
 
 const routes = [
   {
@@ -17,6 +18,20 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from) => {
+  const auth = useAuthStore();
+  if (to.name === "login") {
+    if (auth.isAuth) {
+      return { name: "home" };
+    } else return;
+  }
+});
+
+router.addRoute({
+  path: "/:pathMatch(.*)*",
+  redirect: { name: "home" },
 });
 
 export default router;
